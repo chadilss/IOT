35de42ee590059dc9cd7c2c0806bfd6266132b40
chadilss <chadfinch85@gmail.com>
2018-03-23 22:40:07 +1300

    Update Weather_API.py

--- Weather_API.py
+++ Weather_API.py
@@ -11,12 +11,28 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+moistSensor = 21
+water = false
+noWater = false
+
 #This is just for testing purposes
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
 GPIO.setup(25, GPIO.OUT)
 
+
+
+def checkMoisture(moistSensor):
+	if GPIO.input(moistSensor):
+		noWater = true;
+	else:
+		water = true;
+		
+
+GPIO.add_event_detect(moiseSensor, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(moistSensor, checkMoisture)		
+		
 def receiveTemp():
     "get temperature from weather api"
     APIResponse = urllib2.urlopen('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=62b4ad0fc294f7c579fbf3d3a5b49fb6&q=Auckland')
@@ -29,7 +45,7 @@ w = json.loads(currentWeather)
 # Current Temp
 temp = float(w['list'][0]['main']['temp'])
 temp = (temp - 273) #convert from kelvin to Farenheit
-print ("Current Temp:")
+print ("Current Temp: ")
 print (round(temp))
 
 # Weather Forecast
@@ -62,15 +78,26 @@ print (currentforecast)
 # This will output to the LEDS for testing - this will change once the moisture sensor and relay are in place
 # which then output to the solenoid for water release.
 # we will need to add in a timer which will close the solenoid when the timer expires.
-
-if currentforecast == 'Clear':
-	print ('clear')
-	GPIO.output(18, GPIO.HIGH)
-	time.sleep(1)
-	print ("LED OFF")
-	GPIO.output(18, GPIO.LOW)
-if currentforecast == 'Rain':
-	print ('rain')
+if noWater == true:
+	if currentforecast == 'Clear':
+		print ('Start Water Flow')
+		#tested with an LED
+		GPIO.output(18, GPIO.HIGH)
+		time.sleep(60)
+		print ("Stop Water Flow")
+		GPIO.output(18, GPIO.LOW)
+	elif currentforecast == 'Rain':
+		print ('No water required')
+		GPIO.output(23, GPIO.HIGH)
+		#exit program
+		print ('Rain on the way')
+		exit()
+else:
+	if water == true:
+		#exit program if moisture is detected
+		exit()
+"""
+print ('rain')
 	GPIO.output(23, GPIO.HIGH)
 	time.sleep(1)
 	print ("LED OFF")
@@ -87,3 +114,4 @@ if currentforecast == 'Snow':
 	time.sleep(1)
 	print ("LED OFF")
 	GPIO.output(25, GPIO.LOW)
+"""
